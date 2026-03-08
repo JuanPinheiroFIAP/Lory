f8d7e28ec778d60d443d3dae6c9d18cac82e8fda
Juan Pinheiro de França <rm552202@fiap.com.br>
2026-03-07 23:23:46 -0300

    Refactor message handling in receive_wpp_message

--- src/app/api/v1/mensage.py
+++ src/app/api/v1/mensage.py
@@ -7,11 +7,22 @@ chat_message = APIRouter(prefix="/webhooks", tags=["webhooks"])
 async def receive_wpp_message(request: Request):
     data = await request.json()
 
-    # Verifica se é uma mensagem recebida
+    print("Webhook recebido:", data)
+
     if data.get("event") == "MESSAGES_UPSERT":
-        message_body = data["data"]["message"]["conversation"]
+
+        message_data = data["data"]["message"]
         sender_number = data["data"]["key"]["remoteJid"]
 
-        print(f"Mensagem de {sender_number}: {message_body}")
+        message_body = None
+
+        if "conversation" in message_data:
+            message_body = message_data["conversation"]
+
+        elif "extendedTextMessage" in message_data:
+            message_body = message_data["extendedTextMessage"]["text"]
+
+        if message_body:
+            print(f"Mensagem de {sender_number}: {message_body}")
 
     return {"status": "success"}
