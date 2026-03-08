eac616b932e989056ec621623ed1ff15e598c746
JuanPinheiroFIAP <rm552202@fiap.com.br>
2026-03-07 22:38:39 -0300

    test: reach 100% coverage on core modules

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,14 +4,14 @@ from src.app.main import app
 client = TestClient(app)
 
 
-def test_webhook_endpoint():
+def test_webhook_message_upsert():
     payload = {
+        "event": "MESSAGES_UPSERT",  # Isso faz o código entrar na linha 12
         "data": {
-            "message": {"conversation": "teste"},
+            "message": {"conversation": "Teste de gasto Lory"},
             "key": {"remoteJid": "5511999999999@s.whatsapp.net"},
-        }
+        },
     }
-
     response = client.post("/webhooks/whatsapp", json=payload)
-
     assert response.status_code == 200
+    assert response.json() == {"status": "success"}
